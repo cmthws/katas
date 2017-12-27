8beb0976d4bf034caa98ee61d6948bf14f869fdb
cmthws <33766881+cmthws@users.noreply.github.com>
2017-12-27 15:22:14 -0600

    working PowerShell verion

--- who-likes-it.ps1
+++ who-likes-it.ps1
@@ -1,18 +1,19 @@
 function likes([array]$names)
 {
      
-
     switch(($names.Length, 4 | Measure-Object -Minimum).Minimum)
     {
-        0 { "no one likes this" }
-        1 { "$names[0] likes this" }
-        2 { "$names[0] and $names[1] like this" }
-        3 { $names[0] ", $names[1], and $names[2] like this" }
-        4 { "$names[0], $names[1], and $names.Length-2 others like this" }
+        0 { $out = "no one likes this" }
+        1 { $out = "$names likes this" }
+        2 { $out = "{0} and {1} like this" -f $names[0], $names[1]}
+        3 { $out = "{0}, {1} and {2} like this" -f $names[0], $names[1], $names[2] }
+        4 { $out = "{0}, {1} and {2} others like this" -f $names[0], $names[1], ($names.Length-2).ToString() }
     }
-
-    Write-Host $names.Length
-
+    return $out
 }
 
+likes @()
+likes @("alice")
+likes @("alice","bob")
 likes @("alice","bob","charlie")
+likes @("alice","bob","charlie","dan")
