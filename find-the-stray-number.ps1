772272aa9c177dcfa96101f0ee4924a4c80eee7b
cmthws <33766881+cmthws@users.noreply.github.com>
2018-02-26 14:43:06 -0600

    initial upload of solution to "Find the Stray Number"

---
+++ find-the-stray-number.ps1
@@ -0,0 +1,12 @@
+﻿# https://www.codewars.com/kata/57f609022f4d534f05000024/solutions/python
+
+
+function stray ([array]$arr)
+{
+    $arr | Group-Object -NoElement | Where-Object { $_.Count -eq 1 } | Select-Object -Property Name
+}
+
+$arr1 = @(1, 1, 2)
+$arr2 = @(17, 17, 3, 17, 17, 17, 17)
+
+stray($arr2)
