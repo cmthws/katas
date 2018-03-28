9e4589713e5e73344ad98fe9abdc0fad88efbf6b
cmthws <33766881+cmthws@users.noreply.github.com>
2018-03-28 12:27:20 -0500

    Create moves-in-squared-strings-i.ps1

---
+++ moves-in-squared-strings-i.ps1
@@ -0,0 +1,14 @@
+# Moves in squared strings (I)
+# https://www.codewars.com/kata/56dbe0e313c2f63be4000b25/solutions/python/all/best_practices
+
+###
+# def vert_mirror(s):
+#    return "\n".join(line[::-1] for line in s.split("\n"))
+#
+# def hor_mirror(s):
+#    return "\n".join(s.split("\n")[::-1])
+#
+# def oper(fct, s):
+#    return fct(s)
+###
+
